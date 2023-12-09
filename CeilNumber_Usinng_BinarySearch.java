f44c48c6b5af0a59e2b54ed8da3f0fa7fb4a69b3
QuntamCoder <amoljadhav8832@gmail.com>
2023-12-09 15:21:53 +0530

    this code for fon the ceil of the number in given array

---
+++ CeilNumber_Usinng_BinarySearch.java
@@ -0,0 +1,37 @@
+
+//this program  is same like binary search but in we can use this code to find ceil of the number  
+
+
+public class CeilNumber_Usinng_BinarySearch {
+    public static int BinarySearchAlgo(int tar, int arr[]) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == tar) {
+                // System.out.println("Found at Index "+arr[mid]);
+                return mid;
+            } else if (arr[mid] < tar) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // If the target is not found, you may want to return a special value,
+        // for example, -1 to indicate that the target is not in the array.
+        return arr[left];
+    }
+
+    public static void main(String args[]) {
+        int array[] = {2, 3, 4, 5, 7, 8};
+        int key = 9;
+        int result = BinarySearchAlgo(key, array);
+        if (result != -1) {
+            System.out.println("Target Found at Index " + result);
+        } else {
+            System.out.println("Target not found in the array.");
+        }
+    }
+}
+
+
